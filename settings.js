7c236d44f954596562f03fb9fb8fe4a9a8a38e08
vimamodz <125995272+vimamodz@users.noreply.github.com>
2024-07-21 14:05:44 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -8,7 +8,7 @@ function convertToBool(text, fault = 'true') {
 global.thumb = fs.readFileSync('./lib/bugs/venom.jpg')
 const BOTNAME = 'VAJIRA-MD|-WA-BOT™';
 const FOOTERNAME = '> ＶＡＪＩＲＡ -  ＭＤ - Ｖ4';
-global.owner = process.env.OWNER_NUMBER  || '94701992015' ;  // ADD YOUR NUMBER WITHOUT +
+global.owner = process.env.OWNER_NUMBER  || '94776734030' ;  // ADD YOUR NUMBER WITHOUT +
     
 module.exports = {
 SESSION_ID: process.env.SESSION_ID || 'VAJIRA-MD=KRMjHCpT#ulqK1bP-ii3o5HVTiAMiVhU72rHIuGTBEdw_fT3Y7qc' ,

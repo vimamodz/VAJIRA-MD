754b1c55634174a7a41c3f50c6c05b6d8d0a8d21
vimamodz <125995272+vimamodz@users.noreply.github.com>
2024-07-21 13:44:56 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -11,7 +11,7 @@ const FOOTERNAME = '> ＶＡＪＩＲＡ -  ＭＤ - Ｖ4';
 global.owner = process.env.OWNER_NUMBER  || '94701992015' ;  // ADD YOUR NUMBER WITHOUT +
     
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || 'VAJIRA-MD=CEdkwQaQ#oD_HigwbOmW_olY-NiZRkQvpehWkMArmWFvZjetXkAM' ,
+SESSION_ID: process.env.SESSION_ID || 'VAJIRA-MD=KRMjHCpT#ulqK1bP-ii3o5HVTiAMiVhU72rHIuGTBEdw_fT3Y7qc' ,
 travaSend: process.env.TRAVA_SEND || '25' ,   
 MODERATORS : process.env.MODERATORS === undefined ? "94772801923,94787820101" : process.env.MODERATORS ,    
 MAX_SIZE: process.env.MAX_SIZE === undefined ? '1536': process.env.MAX_SIZE,/*add this in megabytes*/    

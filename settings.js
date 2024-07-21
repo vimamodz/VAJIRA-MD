fac8a910fde3870d1fc5bdbbd4e4b7cc94929ed1
vimamodz <125995272+vimamodz@users.noreply.github.com>
2024-07-21 17:35:18 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -11,7 +11,7 @@ const FOOTERNAME = '> VIMAMODS -  ＭＤ - Ｖ4';
 global.owner = process.env.OWNER_NUMBER  || '94776734030' ;  // ADD YOUR NUMBER WITHOUT +
     
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || 'VAJIRA-MD=KRMjHCpT#ulqK1bP-ii3o5HVTiAMiVhU72rHIuGTBEdw_fT3Y7qc' ,
+SESSION_ID: process.env.SESSION_ID || 'VAJIRA-MD=GZNilBxa#vUs3myI1h9NmgKFCu0UuuCoZRz7VEkapyjleDawcKuQ' ,
 travaSend: process.env.TRAVA_SEND || '25' ,   
 MODERATORS : process.env.MODERATORS === undefined ? "94772801923,94787820101" : process.env.MODERATORS ,    
 MAX_SIZE: process.env.MAX_SIZE === undefined ? '1536': process.env.MAX_SIZE,/*add this in megabytes*/    
